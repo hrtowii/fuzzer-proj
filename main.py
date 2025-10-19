3ef7f8dc17cc431509b500f2ae9232804b79e952
hrtowii <wysi@727.com>
2025-10-19 17:56:04 +0800

    init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# required: setup phase - take arg 1 as a file and check what kind of input it expects, afterwards run mutate backend on it.
+# required: setup phase - take arg 1 as a file, take arg 2 as sample, and check what kind of input it expects, afterwards run mutate backend on it.
 # so functions required: setup() -> detect() -> mutate.run(), where .run() will run diff backends etc. flip bits, whatever
+# midpoint / POC point: take in a binary that accepts csv and json
 def main():
     if len(sys.argv < 2):
         
